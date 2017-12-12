c3e8c0240c77ee20960e5ea34f598dade540bcc3
Dimi Paun <dimi@lattica.com>
2017-12-12 17:39:43 -0500

    Add support for 2/3 components + a suffix.

--- src/main/java/com/lattica/vernum/VersionNumber.java
+++ src/main/java/com/lattica/vernum/VersionNumber.java
@@ -6,40 +6,34 @@ import java.util.regex.Pattern;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
-@Getter
-@EqualsAndHashCode
-@Slf4j
+@Getter @Slf4j
+@EqualsAndHashCode(of = { "major", "minor", "revision", "suffix" })
 public class VersionNumber implements Comparable<VersionNumber> {
-	public static Comparator<VersionNumber> COMPARATOR = new Comparator<VersionNumber>() {
-		@Override
-		public int compare(VersionNumber o1, VersionNumber o2) {
-			return o1.compareTo(o2);
-		}
-	};
+	public static Comparator<VersionNumber> COMPARATOR = VersionNumber::compareTo;
 
 	private int major;
 	private int minor;
-	private int revision;
+	private Integer revision;
 	private String suffix;
 
-	public VersionNumber(int major, int minor, int revision) {
+	public VersionNumber(int major, int minor, Integer revision, String suffix) {
 		this.major = major;
 		this.minor = minor;
 		this.revision = revision;
+		this.suffix = suffix;
 	}
 
-	public VersionNumber(String version) throws ParseException {
+	public VersionNumber(@NonNull String version) throws ParseException {
 		this(version, '.');
 	}
 
-	public VersionNumber(String version, char sep) throws ParseException {
-		if (version == null)
-			throw new NullPointerException("Invalid version");
+	public VersionNumber(@NonNull String version, char sep) throws ParseException {
 		if (version.length() == 0)
-			throw new IllegalArgumentException("Empty version");
-		String[] parts = version.split(Pattern.quote(String.valueOf(sep)));
+			throw new ParseException("Empty version", 0);
+		String[] parts = version.split(Pattern.quote(String.valueOf(sep)), 3);
 		if (parts.length > 0) {
 			try {
 				major = Integer.parseInt(parts[0]);
@@ -50,10 +44,17 @@ public class VersionNumber implements Comparable<VersionNumber> {
 						try {
 							if (last.length() > 0) {
 								revision = Integer.parseInt(last);
+							} else {
+								revision = 0;
 							}
 						} catch (NumberFormatException ex) {
 							int i = 0;
-							while (i < last.length() && Character.isDigit(last.indexOf(i))) i++;
+							for (; i < last.length(); i++) {
+								char c = last.charAt(i);
+								if (!Character.isDigit(c)) {
+									break;
+								}
+							}
 							if (i > 0) {
 								revision = Integer.parseInt(last.substring(0, i));
 							}
@@ -61,51 +62,64 @@ public class VersionNumber implements Comparable<VersionNumber> {
 								suffix = last.substring(i);
 							}
 						}
-						if (parts.length > 3) {
-							throw new java.text.ParseException("version not well formatted: " + version, 0);
-						}
 					}
 				}
 			} catch (NumberFormatException ex) {
-				throw new java.text.ParseException("Empty version number: " + version + " (" + ex.getMessage() + ")",
-						0);
+				throw new ParseException("Empty version number: " + version + " (" + ex.getMessage() + ")", 0);
 			}
 		} else {
 			throw new java.text.ParseException("Empty version number: " + version, 0);
 		}
 	}
 
+	@NonNull
 	public VersionNumber nextRevision() {
-		return new VersionNumber(major, minor, revision + 1);
+		if (revision != null) {
+			return new VersionNumber(major, minor, revision + 1, suffix);
+		} else {
+			return new VersionNumber(major, minor + 1, null, suffix);
+		}
 	}
 
+	@NonNull
 	public VersionNumber prevRevision() throws ParseException {
-		if (revision > 0)
-			return new VersionNumber(major, minor, revision - 1);
-		else if (minor > 0)
-			return new VersionNumber(major, minor - 1, revision);
-		else if (major > 0)
-			return new VersionNumber(major - 1, minor, revision);
-		else
-			throw new java.text.ParseException("Can't set previous version " + toString(), 0);
+		if (revision != null && revision > 0) {
+			return new VersionNumber(major, minor, revision - 1, suffix);
+		} else if (minor > 0) {
+			return new VersionNumber(major, minor - 1, revision, suffix);
+		} else if (major > 0) {
+			return new VersionNumber(major - 1, minor, revision, suffix);
+		} else {
+			throw new ParseException("Can't set previous version " + toString(), 0);
+		}
 	}
 
 	@Override
-	public int compareTo(VersionNumber o) {
-		if (o == null)
-			return 1;
+	public int compareTo(@NonNull VersionNumber o) {
 		if (major != o.major)
 			return major - o.major;
 		if (minor != o.minor)
 			return minor - o.minor;
-		if (revision != o.revision)
-			return revision - o.revision;
+		if (revision != null) {
+			if (o.revision != null) {
+				if (revision != o.revision)
+					return revision - o.revision;
+			} else {
+				return 1;
+			}
+		} else if (o.revision != null) {
+			return -1;
+		}
+		if (suffix != null) {
+			return o.suffix != null ? suffix.compareTo(o.suffix) : 1;
+		}
 		return 0;
 	}
 
 	@Override
+	@NonNull
 	public String toString() {
-		return major + "." + minor + "." + revision + (suffix != null ? suffix : "");
+		return major + "." + minor + (revision != null ? "." + revision : "") + (suffix != null ? suffix : "");
 	}
 
 	public static VersionNumber valueOf(String value) throws ParseException {
@@ -126,10 +140,4 @@ public class VersionNumber implements Comparable<VersionNumber> {
 			return null;
 		}
 	}
-
-	public static VersionNumber fromFilename(String filename) {
-		FilenameParts parts = new FilenameParts(filename);
-		return parts.getVersion();
-	}
-
 }
